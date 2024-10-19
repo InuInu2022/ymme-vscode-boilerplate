006e56449c7f5c8c7652dfdb95031026fb354942
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-10-19 21:06:41 +0900

    feat: 基本テンプレート用の設定を追加

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ymme-vscode-boilerplate
+
 YMM4 plugin boilerplate template repository
+
+- githubのtemplate repositoryで使えるリポジトリです。
+ 	- [テンプレートリポジトリを作成する - GitHub Docs](https://docs.github.com/ja/repositories/creating-and-managing-repositories/creating-a-template-repository)
---
+++ src/Sample/Sample.cs
@@ -0,0 +1,19 @@
+﻿/**
+IPluginを継承する何もしないプラグイン
+
+実際には manju-summoner/YukkuriMovieMaker4PluginSamples を
+参考に適切なプラグインベースを継承して作ってください。
+
+**/
+using YukkuriMovieMaker.Plugin;
+
+namespace Ymm4PluginSample;
+
+//以下のサンプルを参考に作ってください
+//[manju-summoner/YukkuriMovieMaker4PluginSamples: YMM4用プラグインのサンプル集です](https://github.com/manju-summoner/YukkuriMovieMaker4PluginSamples)
+
+[PluginDetails(AuthorName = "", ContentId = "")]
+public class Sample : IPlugin
+{
+    public string Name => "プラグインテンプレート";
+}
